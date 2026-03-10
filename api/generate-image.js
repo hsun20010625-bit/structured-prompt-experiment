aff4fc5e1a4684b83d37c15e6f129557d5eba83a
hsun20010625-bit <hsun20010625@gmail.com>
2026-03-10 16:22:07 +0800

    Add files via upload

--- api/generate-image.js
+++ api/generate-image.js
@@ -34,7 +34,7 @@ export default async function handler(req, res) {
             });
         }
 
-        const MODEL = "imagen-3.0-generate-001";
+        const MODEL = "gemini-2.0-flash-exp-image-generation";
 
         const API_URL =
             `https://generativelanguage.googleapis.com/v1beta/models/${MODEL}:generateContent`;
